1c12ea942e4b9d58d3cfb283a2e37d9e6bfbb1c6
Denis Shchetinkin <evol.deluxe@yandex.ru>
2021-09-13 21:47:34 +0700

    deploy

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,8 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
+  console.log(req)
+
   const token = req.cookies.jwt;
   if (!token) {
     throw new Unauthorized('Необходима авторизация');
